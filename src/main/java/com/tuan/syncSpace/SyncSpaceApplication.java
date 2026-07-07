2a7058819615bb8f158c81cad2e3ef51260b3878
TuanDangDuc <tdang9078@gmail.com>
2026-07-07 15:26:08 +0700

    init prj

---
+++ src/main/java/com/tuan/syncSpace/SyncSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.tuan.syncSpace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncSpaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tuan/syncSpace/SyncSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuan.syncSpace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
